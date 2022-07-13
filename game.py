6dbd2dceef316a892e71d52460a3f3ad8bf195f4
Misbah Talha <108705015+mt4230@users.noreply.github.com>
2022-07-13 12:17:59 -0400

    Update game.py
    Winner determination

--- game.py
+++ game.py
@@ -27,6 +27,43 @@ print (f"Computer chose:'{computer_choice}'")
 
 # DETERMINE THE WINNER
 
+# adapted from code shared in slack by Bonnie Auger at 08:38 pm on July 13, 2022
+# https://nyu-tech-2335.slack.com/archives/C5WPFSB52/p1657672686150239
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # DISPLAY RESULTS
 
