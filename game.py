97fb0425d3c65625c0aed9cde7c6a476ffef1e81
Misbah Talha <108705015+mt4230@users.noreply.github.com>
2022-07-13 10:43:36 -0400

    Make a plan

--- game.py
+++ game.py
@@ -1,4 +1,33 @@
 # this is the "game.py" file...
 
+from click import Choice
+
+
 print("Rock, Paper, Scissors, Shoot!")
 print("WELCOME TO ROCK PAPER SCISSORS GAME")
+
+
+# USER INPUTS
+
+
+# VALIDATE USER INPUTS
+
+
+# COMPUTER CHOICE
+
+
+# DETERMINE THE WINNER
+
+
+# DISPLAY RESULTS
+
+# -------------------
+# Welcome 'Player One' to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
