89d042729662ae02b716050d1f9a6185bdb34f54
Misbah Talha <108705015+mt4230@users.noreply.github.com>
2022-07-13 10:54:51 -0400

    Update game.py
    User Choice

--- game.py
+++ game.py
@@ -1,13 +1,18 @@
-# this is the "game.py" file...
-
-from click import Choice
 
 
-print("Rock, Paper, Scissors, Shoot!")
+
 print("WELCOME TO ROCK PAPER SCISSORS GAME")
 
 
 # USER INPUTS
+
+
+user_choice = input ("Please make a selection ('rock', 'paper', 'scissors'):")
+
+# You chose: 'rock'
+print (f"You chose:'{user_choice}'")
+
+
 
 
 # VALIDATE USER INPUTS
